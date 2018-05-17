c2827c5219d2bfa2a06413bd8ed506d271bdba95
silver <rjkadmin@163.com>
2018-05-18 01:46:21 +0800

    更新首页消息

--- src/components/Overview/components/Schedule.jsx
+++ src/components/Overview/components/Schedule.jsx
@@ -5,27 +5,19 @@ import Title from './Title';
 
 export default class HeaderView extends Component {
   render() {
+    const { msgList } = this.props;
+    // console.log(msgList);
+    const list = msgList ? msgList.map((el) => <div className={styles.item}>
+      <div className={styles.title} >【{el.siteMsg.title}】</div>
+      {/* <div className={styles['content-title']}></div> */}
+      <div className={styles.content}>{el.siteMsg.content}</div>
+      <div className={styles.time}>{el.siteMsg.gmtCreate}</div>
+    </div>) : [];
     return (
       <div className={styles.schedule}>
         <Title title="待办事项" icon="sync" />
         <div className={styles['schedule-content']}>
-          <div className={styles.item}>
-            <div className={styles.title} >【发现新品审核】</div>
-            <div className={styles['content-title']}>买手belig，提交最新发现商品</div>
-            <div className={styles.content}>女款西服，采购有效时间为48小时，请尽快审核。</div>
-            <div className={styles.time}>2018/04/29 12:33:00</div>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.title} >【采购任务单待分配】</div>
-            <div className={styles['content-title']}>订单号：BS103938493，待分配</div>
-            <div className={styles.time}>2018/04/29 12:33:00</div>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.title} >【采购任务待接收】</div>
-            <div className={styles['content-title']}>订单号：BS103938493，已分配给你采购</div>
-            <div className={styles.content}>请前往订单列表查看详情，也可以打开采购小程序，直接采购。</div>
-            <div className={styles.time}>2018/04/29 12:33:00</div>
-          </div>
+        {list}
         </div>
       </div>
     );
